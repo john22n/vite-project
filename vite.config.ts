2e4e755737cb2065de3423af349188d90f033e26
John Noriega <johnnoriega22@icloud.com>
2022-12-26 14:01:55 -0600

    setup project files

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+/// <refernce type="vitest" />
+/// <refernce type="vite/client" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -5,4 +8,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     base: '/vite-project/',
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: ['./src/setup.ts']
+    },
 })
